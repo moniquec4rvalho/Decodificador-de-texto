1712ceb09cbe000bafab65a2aceebdd4206dfca3
moniquec4rvalho <101936682+moniquec4rvalho@users.noreply.github.com>
2023-01-11 19:56:02 -0300

    Changes

--- Chalenge/criptografador.js
+++ Chalenge/criptografador.js
@@ -11,3 +11,26 @@ function cript() {
 
   criptDone.innerHTML = textocriptografado;
 }
+
+function unCript() {
+  let texto = document.querySelector("#valueToCript");
+  let textoDescriptografado = texto.value
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+
+  let criptDone = document.querySelector("#textCripted");
+
+  criptDone.innerHTML = textoDescriptografado;
+}
+
+function myFunction() {
+  var x = document.getElementById("#textCripted");
+  if (x.style.display === "#copy") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}

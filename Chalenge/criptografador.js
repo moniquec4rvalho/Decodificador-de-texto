34d5f82913266fc157e97e6b985df6af976dd8d6
moniquec4rvalho <101936682+moniquec4rvalho@users.noreply.github.com>
2023-01-11 21:47:05 -0300

    Pronto.

--- Chalenge/criptografador.js
+++ Chalenge/criptografador.js
@@ -1,6 +1,6 @@
 function cript() {
   let texto = document.querySelector("#valueToCript");
-  let textocriptografado = texto.value
+  let textocriptografado = texto.value.toLowerCase()
     .replaceAll("e", "enter")
     .replaceAll("i", "imes")
     .replaceAll("a", "ai")
@@ -8,13 +8,15 @@ function cript() {
     .replaceAll("u", "ufat");
 
   let criptDone = document.querySelector("#textCripted");
-
+  
+  hideCardContent();
+  showResult();
   criptDone.innerHTML = textocriptografado;
 }
 
 function unCript() {
   let texto = document.querySelector("#valueToCript");
-  let textoDescriptografado = texto.value
+  let textoDescriptografado = texto.value.toLowerCase()
     .replaceAll("enter", "e")
     .replaceAll("imes", "i")
     .replaceAll("ai", "a")
@@ -23,14 +25,32 @@ function unCript() {
 
   let criptDone = document.querySelector("#textCripted");
 
+  hideCardContent();
+  showResult();
   criptDone.innerHTML = textoDescriptografado;
 }
 
-function myFunction() {
-  var x = document.getElementById("#textCripted");
-  if (x.style.display === "#copy") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+function hideCardContent() {
+  let cardContent = document.querySelector("#cardCont");
+  cardContent.style.display = "none";
 }
+
+function showResult() {
+  let result = document.querySelector("#text-result");
+  result.style.display = "flex";
+  showCopy();
+}
+
+function showCopy(){
+  let copyButton = document.querySelector("#buttonBox");
+  copyButton.style.display = "flex";
+}
+
+
+
+function copyText(){
+  let texResult = document.querySelector("#textCripted");
+  navigator.clipboard.writeText(texResult.innerHTML);
+  alert("Texto copiado!");
+}
+
